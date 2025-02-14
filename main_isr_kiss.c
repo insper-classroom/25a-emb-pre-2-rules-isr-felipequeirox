08560888857102ddbf66ad62ad5c264600341205
Felipe <fepodqueiroz@gmail.com>
2025-02-14 18:18:01 -0300

    Teste 2.1

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -2,12 +2,10 @@
 
 volatile int g_cnt = 0;
 char g_str[10];
-int batata;
 
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
 void btn_callback(void) {
-  printf("btn pressed \n");
 
   if (g_cnt >= 8)
     g_cnt = 0;
@@ -19,6 +17,7 @@ void btn_callback(void) {
 
 void main(void) {
   // ...
+  int batata;
 
   batata = batata + 1;
   while (1) {
